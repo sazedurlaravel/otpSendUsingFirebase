9927249113ca34bf9457b872b181a0dcd9a7a0a9
Computer Vision <sazedur936@gmail.com>
2021-05-27 10:04:32 +0600

    phone verification using firebase

---
+++ resources/views/home.blade.php
@@ -0,0 +1,116 @@
+<html>
+    <head>
+        <title>Laravel 8 Mobile Number (OTP) Authentication using Firebase</title>    
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    </head>
+<body>  
+<div class="container">
+    <h3 style="margin-top: 30px;">Laravel 8 Mobile Number (OTP) Authentication using Firebase - websolutionstuff.com</h3><br>  
+    <div class="alert alert-danger" id="error" style="display: none;"></div>  
+    <div class="card">
+      <div class="card-header">
+        Enter Phone Number
+      </div>
+      <div class="card-body">  
+        <div class="alert alert-success" id="sentSuccess" style="display: none;"></div>
+        <form>
+            <label>Phone Number:</label>
+            <input type="text" id="number" class="form-control" placeholder="+91 9876543210"><br>
+            <div id="recaptcha-container"></div><br>
+            <button type="button" class="btn btn-success" onclick="SendCode();">Send Code</button>
+        </form>
+      </div>
+    </div>
+      
+    <div class="card" style="margin-top: 10px">
+      <div class="card-header">
+        Enter Verification code
+      </div>
+      <div class="card-body">
+        <div class="alert alert-success" id="successRegsiter" style="display: none;"></div>
+        <form>
+            <input type="text" id="verificationCode" class="form-control" placeholder="Enter Verification Code"><br>
+            <button type="button" class="btn btn-success" onclick="VerifyCode();">Verify Code</button>
+        </form>
+      </div>
+    </div>
+  
+</div>
+  
+<script src="https://www.gstatic.com/firebasejs/6.0.2/firebase.js"></script>
+  
+<script>
+      
+  var firebaseConfig = {
+    apiKey: "AIzaSyBi2bBjAPvZZqapNLp2s3FT0TOSdEfqs38",
+    authDomain: "otp-send-feee3.firebaseapp.com",
+    projectId: "otp-send-feee3",
+    storageBucket: "otp-send-feee3.appspot.com",
+    messagingSenderId: "866329308497",
+    appId: "1:866329308497:web:d7b899020002906e13f770",
+    measurementId: "G-15196CGKXK"
+
+    // apiKey: "XXXX",
+    // authDomain: "XXXX.firebaseapp.com",
+    // databaseURL: "XXXX.firebaseio.com",
+    // projectId: "XXXX",
+    // storageBucket: "XXXX.appspot.com",
+    // messagingSenderId: "XXXX",
+    // appId: "XXXX",
+    // measurementId: "XXXX"
+  };
+    
+  firebase.initializeApp(firebaseConfig);
+</script>
+  
+<script type="text/javascript">
+  
+    window.onload=function () {
+      render();
+    };
+  
+    function render() {
+        window.recaptchaVerifier=new firebase.auth.RecaptchaVerifier('recaptcha-container');
+        recaptchaVerifier.render();
+    }
+  
+    function SendCode() {
+           
+        var number = $("#number").val();
+          
+        firebase.auth().signInWithPhoneNumber(number,window.recaptchaVerifier).then(function (confirmationResult) {
+              
+            window.confirmationResult=confirmationResult;
+            coderesult=confirmationResult;            
+  
+            $("#sentSuccess").text("Message Sent Successfully.");
+            $("#sentSuccess").show();
+              
+        }).catch(function (error) {
+            $("#error").text(error.message);
+            $("#error").show();
+        });
+  
+    }
+  
+    function VerifyCode() {
+  
+        var code = $("#verificationCode").val();
+  
+        coderesult.confirm(code).then(function (result) {
+            var user=result.user;            
+  
+            $("#successRegsiter").text("You Are Register Successfully.");
+            $("#successRegsiter").show();
+  
+        }).catch(function (error) {
+            $("#error").text(error.message);
+            $("#error").show();
+        });
+    }
+  
+</script>
+  
+</body>
+</html>
